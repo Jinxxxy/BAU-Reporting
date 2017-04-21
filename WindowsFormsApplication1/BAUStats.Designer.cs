7ac31e563abb57641756525a6e10ec579ce24fe5
Craig Berry <craigb@cccs.co.uk>
2017-04-21 16:46:58 +0100

    Ready for first commit

---
+++ WindowsFormsApplication1/BAUStats.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.PasswordInput = new System.Windows.Forms.TextBox();
+            this.UsernameInput = new System.Windows.Forms.TextBox();
+            this.HeaderLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(12, 69);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(116, 13);
+            this.UsernameLabel.TabIndex = 0;
+            this.UsernameLabel.Text = "ServiceNow Username";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(12, 109);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 1;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(15, 153);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 2;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // PasswordInput
+            // 
+            this.PasswordInput.Location = new System.Drawing.Point(134, 106);
+            this.PasswordInput.Name = "PasswordInput";
+            this.PasswordInput.PasswordChar = '*';
+            this.PasswordInput.Size = new System.Drawing.Size(258, 20);
+            this.PasswordInput.TabIndex = 3;
+            this.PasswordInput.UseSystemPasswordChar = true;
+            // 
+            // UsernameInput
+            // 
+            this.UsernameInput.Location = new System.Drawing.Point(134, 66);
+            this.UsernameInput.Name = "UsernameInput";
+            this.UsernameInput.Size = new System.Drawing.Size(258, 20);
+            this.UsernameInput.TabIndex = 4;
+            // 
+            // HeaderLabel
+            // 
+            this.HeaderLabel.AutoSize = true;
+            this.HeaderLabel.Location = new System.Drawing.Point(12, 9);
+            this.HeaderLabel.Name = "HeaderLabel";
+            this.HeaderLabel.Size = new System.Drawing.Size(136, 13);
+            this.HeaderLabel.TabIndex = 5;
+            this.HeaderLabel.Text = "BAU Montly Reporting Tool";
+            // 
+            // LoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 523);
+            this.Controls.Add(this.HeaderLabel);
+            this.Controls.Add(this.UsernameInput);
+            this.Controls.Add(this.PasswordInput);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Name = "LoginScreen";
+            this.Text = "BAU Monthly Reporting Tool ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox PasswordInput;
+        private System.Windows.Forms.TextBox UsernameInput;
+        private System.Windows.Forms.Label HeaderLabel;
+    }
+}
+
---
+++ WindowsFormsApplication1/BAUStats.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class LoginScreen : Form
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+        public string GetUserName(TextBox _usernameInput)
+        {
+            string rawUsername = _usernameInput.Text;
+            Boolean blank = isStringNotBlank(rawUsername);
+            MessageBox.Show(rawUsername);
+            MessageBox.Show(this.isStringNotBlank(rawUsername).ToString());
+            if (blank)
+            {
+                MessageBox.Show("This is not blank");
+                return rawUsername;
+            } else
+            {
+                MessageBox.Show("This is blank");
+                return rawUsername;
+            }
+            
+
+
+        }
+
+        private string getPassword(TextBox _passwordInput)
+        {
+            string rawPassword = _passwordInput.Text;
+            bool isPasswordNotBlank = isStringNotBlank(rawPassword);
+            if(isPasswordNotBlank)
+            {
+                return rawPassword;
+            } else
+            {
+                return rawPassword;
+            }
+        }
+        private Boolean isStringNotBlank(string item)
+        {
+            if(item.Length <= 0)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+
+        public async Task<string> MakeRequestToServiceNow()
+        {
+            //Testing
+            string username = GetUserName(this.UsernameInput);
+            string password = getPassword(this.PasswordInput);
+            //Testing
+                        
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+                );
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "basic",
+                Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password))
+                );
+            HttpResponseMessage response = await client.GetAsync("https://stepchangeprod.service-now.com/api/now/table/incident?sysparm_limit=10&assignment_group=Dev%20-%20Advice%20%26%20Activations");
+            
+            var returnValue = response.Content.ReadAsStringAsync();
+
+            return returnValue.ToString();
+
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            MakeRequestToServiceNow();
+        }
+    }
+}
