cb5a32daa43acf780eba429a1d67b3ea8bbd0b0f
Craig Berry <craigb@cccs.co.uk>
2017-04-28 12:23:22 +0100

    Transfer Commit

--- WindowsFormsApplication1/Services/GettingServiceDetails.cs
+++ WindowsFormsApplication1/Services/GettingServiceDetails.cs
@@ -1,21 +1,152 @@
-﻿using Newtonsoft.Json;
+﻿using Library.Forms;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsApplication1;
+
+namespace Library.Forms
+{
+    /// <summary>
+    /// Provides a generic collection that supports data binding and additionally supports sorting.
+    /// See http://msdn.microsoft.com/en-us/library/ms993236.aspx
+    /// If the elements are IComparable it uses that; otherwise compares the ToString()
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    public class SortableBindingList<T> : BindingList<T> where T : class
+    {
+        private bool _isSorted;
+        private ListSortDirection _sortDirection = ListSortDirection.Ascending;
+        private PropertyDescriptor _sortProperty;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SortableBindingList{T}"/> class.
+        /// </summary>
+        public SortableBindingList()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SortableBindingList{T}"/> class.
+        /// </summary>
+        /// <param name="list">An <see cref="T:System.Collections.Generic.IList`1" /> of items to be contained in the <see cref="T:System.ComponentModel.BindingList`1" />.</param>
+        public SortableBindingList(IList<T> list)
+            : base(list)
+        {
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the list supports sorting.
+        /// </summary>
+        protected override bool SupportsSortingCore
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the list is sorted.
+        /// </summary>
+        protected override bool IsSortedCore
+        {
+            get { return _isSorted; }
+        }
+
+        /// <summary>
+        /// Gets the direction the list is sorted.
+        /// </summary>
+        protected override ListSortDirection SortDirectionCore
+        {
+            get { return _sortDirection; }
+        }
+
+        /// <summary>
+        /// Gets the property descriptor that is used for sorting the list if sorting is implemented in a derived class; otherwise, returns null
+        /// </summary>
+        protected override PropertyDescriptor SortPropertyCore
+        {
+            get { return _sortProperty; }
+        }
+
+        /// <summary>
+        /// Removes any sort applied with ApplySortCore if sorting is implemented
+        /// </summary>
+        protected override void RemoveSortCore()
+        {
+            _sortDirection = ListSortDirection.Ascending;
+            _sortProperty = null;
+            _isSorted = false; //thanks Luca
+        }
+
+        /// <summary>
+        /// Sorts the items if overridden in a derived class
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <param name="direction"></param>
+        protected override void ApplySortCore(PropertyDescriptor prop, ListSortDirection direction)
+        {
+            _sortProperty = prop;
+            _sortDirection = direction;
+
+            List<T> list = Items as List<T>;
+            if (list == null) return;
+
+            list.Sort(Compare);
+
+            _isSorted = true;
+            //fire an event that the list has been changed.
+            OnListChanged(new ListChangedEventArgs(ListChangedType.Reset, -1));
+        }
+
+
+        private int Compare(T lhs, T rhs)
+        {
+            var result = OnComparison(lhs, rhs);
+            //invert if descending
+            if (_sortDirection == ListSortDirection.Descending)
+                result = -result;
+            return result;
+        }
+
+        private int OnComparison(T lhs, T rhs)
+        {
+            object lhsValue = lhs == null ? null : _sortProperty.GetValue(lhs);
+            object rhsValue = rhs == null ? null : _sortProperty.GetValue(rhs);
+            if (lhsValue == null)
+            {
+                return (rhsValue == null) ? 0 : -1; //nulls are equal
+            }
+            if (rhsValue == null)
+            {
+                return 1; //first has value, second doesn't
+            }
+            if (lhsValue is IComparable)
+            {
+                return ((IComparable)lhsValue).CompareTo(rhsValue);
+            }
+            if (lhsValue.Equals(rhsValue))
+            {
+                return 0; //both are the same
+            }
+            //not comparable, compare ToString
+            return lhsValue.ToString().CompareTo(rhsValue.ToString());
+        }
+    }
+}
 
 namespace WindowsFormsApplication1.Services
 {
     class GettingServiceDetails
     {
         public static Dictionary<string, string> loginDetails =  new Dictionary<string,string>();
-        public static List<IncidentClass> incidentList = new List<IncidentClass>();
+        public static SortableBindingList<IncidentClass> incidentList = new SortableBindingList<IncidentClass>();
         public string MakeRequestToServiceNow(string _url)
         {
             string username = GettingServiceDetails.loginDetails["userName"];
@@ -55,13 +186,13 @@ namespace WindowsFormsApplication1.Services
             }
             return result;
         }
-        public List<IncidentClass> serializeString(string _returnedString)
+        public SortableBindingList<IncidentClass> serializeString(string _returnedString)
         {
             JToken obj = JObject.Parse(_returnedString)["result"];
             string newString = JsonConvert.SerializeObject(obj);
             try
             {
-                List<IncidentClass> dataSource = JsonConvert.DeserializeObject<List<IncidentClass>>(newString);
+                SortableBindingList<IncidentClass> dataSource = JsonConvert.DeserializeObject<SortableBindingList<IncidentClass>>(newString);
                 GettingServiceDetails.incidentList = dataSource;            
                 return dataSource;
             }
@@ -72,19 +203,26 @@ namespace WindowsFormsApplication1.Services
                 throw err;
             }
         }
-        public List<string> getApplicationNames(List<IncidentClass> incList)
+        public object GetPropValue(IncidentClass src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src);
+        }
+        public List<string> getXAxisNames(SortableBindingList<IncidentClass> incList, string labelName)
         {
             List<string> nameList = new List<string>();
             foreach(IncidentClass i in incList)
             {
-                if (nameList.IndexOf(i.Application) < 0)
+                PropertyInfo prop = typeof(IncidentClass).GetProperty(labelName);
+                object value = prop.GetValue(i, null);
+                
+                if (nameList.IndexOf(value.ToString()) < 0)
                 {
-                    nameList.Add(i.Application);
+                    nameList.Add(value.ToString());
                 }
             }
             return nameList;
         } 
-        public Dictionary<string,int> getApplicationNumbers(List<IncidentClass> incList, List<string> appNames)
+        public Dictionary<string,int> getYAxisNumbers(SortableBindingList<IncidentClass> incList, List<string> appNames, string propName)
         {
             Dictionary<string, int> appNumbers = new Dictionary<string, int>();
             foreach(string app in appNames)
@@ -93,7 +231,8 @@ namespace WindowsFormsApplication1.Services
                 int appCount = 0;
                 foreach(IncidentClass inc in incList)
                 {
-                    if(inc.Application == name)
+                    var incPropName = GetPropValue(inc, propName).ToString();
+                    if(incPropName == name)
                     {
                         appCount++;
                     }
---
+++ WindowsFormsApplication1/Services/RequestStringBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WindowsFormsApplication1.Services
+{
+    class RequestStringBuilder
+    {
+
+        private string baseString = "https://stepchangeprod.service-now.com/api/now/table/incident?";
+        private string projection = "sysparm_fields=cmdb_ci.name,assigned_to.name,closed_at,sys_created_on,u_closure_code,u_closure_sub_code,assignment_group.name,description,short_description,priority,number";
+        private string separator = "&";
+        private string teamSelector = "assignment_group=";
+        private string queryOpener = "sysparm_query=";
+        private string closedConditionString = "incident_state=7";
+        public static Dictionary<string, string> teamDictionary = new Dictionary<string, string>();        
+        private string startDateStringBuilder(string[] startDateArr)
+        {
+            string field = "closed_at>=";
+            string vanityDate = startDateArr[2] + "-" + startDateArr[1] + "-" + startDateArr[0];
+            string openScript = "@javascript:gs.dateGenerate('";
+            string endScript = "','00:00:00')";
+
+
+            return field + vanityDate + openScript + vanityDate + endScript;            
+        }
+        private string endDateStringBuilder(string[] endDateArr)
+        {
+            string field = "closed_at<=";
+            string vanityDate = endDateArr[2] + "-" + endDateArr[1] + "-" + endDateArr[0];
+            string openScript = "@javascript:gs.dateGenerate('";
+            string endScript = "','23:59:59')";
+            return field + vanityDate + openScript + vanityDate + endScript;
+        }
+        public string getRequestString(string[] _start, string[] _end, string teamName)
+        {
+            
+            var startDate = startDateStringBuilder(_start);
+            var endDate = endDateStringBuilder(_end);
+            var _teamName = Uri.EscapeUriString(teamName).Replace("&", "%26");
+            var queryString = queryOpener + startDate + separator + endDate + separator + closedConditionString + separator + teamSelector;
+            
+            var test2 = baseString  + projection + separator + queryString + _teamName;
+            return test2;
+        }
+    }
+   
+
+}
